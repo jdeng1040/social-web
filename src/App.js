47dad936942ebc8da45bf01e9083201413d87f78
jdeng1040 <73567329+jdeng1040@users.noreply.github.com>
2021-06-10 20:51:54 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>Main</h1>
+      <Header />
+      <h1>Main</h1>
+    </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <div>
+            <nav>
+                <li>directory1</li>
+                <li>directory2</li>
+            </nav>
+        </div>
+    )
+}
